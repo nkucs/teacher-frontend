8e3ba76ee182da0b3d7ab643aec0da9d70e6c95d
yanqingdi <wx_d46b7b2598ab49f28501365f96e41976@git.code.tencent.com>
2019-05-23 22:22:19 +0800

    feat: Add exam student(#6)

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -14,72 +14,78 @@ export const asyncRouterMap = [
         path: '/course',
         name: '课程管理',
         component: () => import('@/views/course/Course'),
-        meta: { title: '课程管理', icon: 'dashboard', permission: [ 'dashboard' ] }
+        meta: { title: '课程管理', icon: 'dashboard', permission: ['dashboard'] }
       },
       {
         path: '/problem/list',
         name: '题目管理',
         component: PageView,
-        meta: { title: '题目管理', icon: 'dashboard', permission: [ 'dashboard' ] },
+        meta: { title: '题目管理', icon: 'dashboard', permission: ['dashboard'] },
         children: [{
           path: '/problem/list', // todo
           name: '题目列表',
           component: () => import('@/views/problem/ProblemList'),
-          meta: { title: '题目列表', permission: [ 'dashboard' ] }
+          meta: { title: '题目列表', permission: ['dashboard'] }
         },
         {
           path: '/problem/add', // todo
           name: '添加题目',
           component: () => import('@/views/problem/ProblemDetail'),
-          meta: { title: '添加题目', permission: [ 'dashboard' ] }
-        },{
+          meta: { title: '添加题目', permission: ['dashboard'] }
+        }, {
           path: '/problem/modify', // todo
           name: '修改题目',
           component: () => import('@/views/problem/ProblemModify'),
-          meta: { title: '修改题目', permission: [ 'dashboard' ] }
+          meta: { title: '修改题目', permission: ['dashboard'] }
         }]
       },
       {
         path: '/stat/course',
         name: '统计信息',
         component: PageView,
-        meta: { title: '统计信息', icon: 'dashboard', permission: [ 'dashboard' ] },
+        meta: { title: '统计信息', icon: 'dashboard', permission: ['dashboard'] },
         children: [{
           path: '/stat/course', // todo
           name: '课程统计',
           component: () => import('@/views/stat/StatCourse'),
-          meta: { title: '课程统计', permission: [ 'dashboard' ] }
+          meta: { title: '课程统计', permission: ['dashboard'] }
         },
         {
           path: '/stat/problem', // todo
           name: '题目分析',
           component: () => import('@/views/stat/StatProblem'),
-          meta: { title: '题目分析', permission: [ 'dashboard' ] }
+          meta: { title: '题目分析', permission: ['dashboard'] }
         },
         {
           path: '/stat/exam', // todo
           name: '考试情况',
           component: () => import('@/views/stat/StatExam'),
-          meta: { title: '考试情况', permission: [ 'dashboard' ] }
+          meta: { title: '考试情况', permission: ['dashboard'] }
         }]
       },
       {
         path: '/exam/list',
         name: '考试中心',
         component: BlankLayout,
-        meta: { title: '考试中心', icon: 'dashboard', permission: [ 'dashboard' ] },
+        meta: { title: '考试中心', icon: 'dashboard', permission: ['dashboard'] },
         children: [{
           path: '/exam/addproblems', // todo
           name: '添加考试题目',
           component: () => import('@/views/exam/ExamAddProblems'),
-          meta: { title: '添加考试题目'}
+          meta: { title: '添加考试题目' }
+        },
+        {
+          path: '/exam/addstudent', // todo
+          name: '添加考试学生',
+          component: () => import('@/views/exam/ExamAddStudent'),
+          meta: { title: '添加考试学生' }
         }]
       },
       {
         path: '/experiment/list',
         name: '实验列表',
         component: () => import('@/views/experiment/ExperimentList'),
-        meta: { title: '实验列表', icon: 'dashboard', permission: [ 'dashboard' ] }
+        meta: { title: '实验列表', icon: 'dashboard', permission: ['dashboard'] }
       }
     ]
   },
