df59b5da5192a6209d720372134d9fd768132114
nankaielsasong <nankai_elsasong@163.com>
2019-05-25 01:10:51 +0800

    style: fix lint errors and warnings (#3)

--- src/views/experiment/CreateExperiment.vue
+++ src/views/experiment/CreateExperiment.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
-    <a-modal title="确认取消" :visible="cancelSubmitVisible" @ok="confirmCancel" :confirmLoading="confirmLoading"
+    <a-modal 
+      title="确认取消" 
+      :visible="cancelSubmitVisible" 
+      @ok="confirmCancel" 
+      :confirmLoading="confirmLoading"
       @cancel="abandonCancel">
-      <p>{{cancelModalText}}</p>
+      <p>{{ cancelModalText }}</p>
     </a-modal>
 
-    <a-modal title="确认新建" :visible="submitVisible" @ok="confirmSubmit" :confirmLoading="confirmLoading"
+    <a-modal 
+      title="确认新建" 
+      :visible="submitVisible" 
+      @ok="confirmSubmit" 
+      :confirmLoading="confirmLoading"
       @cancel="cancelSubmit">
-      <p>{{submitModalText}}</p>
+      <p>{{ submitModalText }}</p>
     </a-modal>
 
-    <a-modal title="添加练习题目" :visible="exerciseAdditionVisible" @ok="confirmAdd" :confirmLoading="confirmLoading"
+    <a-modal 
+      title="添加练习题目" 
+      :visible="exerciseAdditionVisible" 
+      @ok="confirmAdd" 
+      :confirmLoading="confirmLoading"
       @cancel="cancelAdd">
     </a-modal>
 
@@ -21,12 +33,16 @@
         </a-button>
       </a-form-item>
       <a-form-item v-bind="formItemLayout" label="开始时间：">
-        <a-date-picker v-decorator="['start-date-time-picker', datePickerConfig]" show-time
+        <a-date-picker 
+          v-decorator="['start-date-time-picker', datePickerConfig]" 
+          show-time
           format="YYYY-MM-DD HH:mm:ss" />
       </a-form-item>
 
       <a-form-item v-bind="formItemLayout" label="截止时间：">
-        <a-date-picker v-decorator="['end-date-time-picker', datePickerConfig]" show-time
+        <a-date-picker 
+          v-decorator="['end-date-time-picker', datePickerConfig]" 
+          show-time
           format="YYYY-MM-DD HH:mm:ss" />
       </a-form-item>
 
@@ -44,9 +60,9 @@
         <a-button class="formButton" @click="handleCancel">
           取消
         </a-button>
-          <a-button class="formButton" type="primary" html-type="submit">
-            创建
-          </a-button>
+        <a-button class="formButton" type="primary" html-type="submit">
+          创建
+        </a-button>
       </a-form-item>
     </a-form>
   </div>
@@ -79,7 +95,7 @@
       radioConfig: {
         rules: [{
           required: true,
-          message: "请确定是否需要提交实验报告！"
+          message: '请确定是否需要提交实验报告！'
         }]
       },
       formValues: null,
@@ -92,64 +108,64 @@
 
     }),
     beforeCreate() {
-      this.form = this.$form.createForm(this);
+      this.form = this.$form.createForm(this)
     },
     methods: {
       addExercises() {
-        this.exerciseAdditionVisible = true;
+        this.exerciseAdditionVisible = true
       },
       handleCancel() {
-        this.cancelSubmitVisible = true;
+        this.cancelSubmitVisible = true
       },
       handleSubmit(e) {
-        e.preventDefault();
+        e.preventDefault()
         this.form.validateFields((err, values) => {
           if (!err) {
             // get values from form
-            this.submitVisible = true;
-            this.formValues = values;
+            this.submitVisible = true
+            this.formValues = values
           }
-        });
+        })
       },
       confirmAdd() {
-        this.confirmLoading = true;
+        this.confirmLoading = true
         setTimeout(() => {
-          this.exerciseAdditionVisible = false;
-          this.confirmLoading = false;
-        }, 500);
+          this.exerciseAdditionVisible = false
+          this.confirmLoading = false
+        }, 500)
       },
 
       cancelAdd() {
-        this.exerciseAdditionVisible = false;
+        this.exerciseAdditionVisible = false
       },
 
       confirmSubmit() {
         // 开始上传数据
-        console.log(this.formValues);
-        this.confirmLoading = true;
+        console.log(this.formValues)
+        this.confirmLoading = true
         setTimeout(() => {
-          this.submitVisible = false;
-          this.confirmLoading = false;
-        }, 500);
+          this.submitVisible = false
+          this.confirmLoading = false
+        }, 500)
       },
 
       cancelSubmit() {
-        console.log("check again");
-        this.submitVisible = false;
+        console.log('check again')
+        this.submitVisible = false
       },
 
       confirmCancel() {
-        this.confirmLoading = true;
+        this.confirmLoading = true
         setTimeout(() => {
-          this.cancelSubmitVisible = false;
-          this.confirmLoading = false;
-        }, 500);
+          this.cancelSubmitVisible = false
+          this.confirmLoading = false
+        }, 500)
       },
-      abandonCancel(e) {
+      abandonCancel() {
         this.cancelSubmitVisible = false
       },
     },
-  };
+  }
 </script>
 <style>
   #components-form-demo-validate-other .dropbox {
