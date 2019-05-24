9c49f159ead91e07ca55f519b845d83adaf49817
Shawn Xu <xuuexu@gmail.com>
2019-05-24 21:50:18 +0800

    reformat code and delete extra semicolon to remove error #33

--- src/views/problem/EditableCell.vue
+++ src/views/problem/EditableCell.vue
@@ -30,26 +30,26 @@
 <script>
 export default {
   props: {
-    text: String,
+    text: String('Default')
   },
   data () {
     return {
       value: this.text,
       editable: false,
-    };
+    }
   },
   methods: {
     handleChange (e) {
-      const value = e.target.value;
-      this.value = value;
+      const value = e.target.value
+      this.value = value
     },
     check () {
-      this.editable = false;
-      this.$emit('change', this.value);
+      this.editable = false
+      this.$emit('change', this.value)
     },
     edit () {
-      this.editable = true;
+      this.editable = true
     },
   },
-};
+}
 </script>
