989e4d5221cd8b29a0bbc4e26c3a268e76b00ecd
Bojan <qc_60c537f462dc46f3bacab79e3535a173@git.code.tencent.com>
2019-05-24 20:31:29 +0800

    feat: 统计概览饼图

--- src/views/stat/StatOverview.vue
+++ src/views/stat/StatOverview.vue
@@ -1,29 +1,10 @@
 <template>
   <div class="page-header-index-wide">
-    <a-row :gutter="12">
-      <a-col :xl="12" :lg="24" :md="24" :sm="24" :xs="24">
-        <a-card :loading="loading" :bordered="false" title="线上热门搜索" :style="{ marginTop: '24px' }">
-          <a-dropdown :trigger="['click']" placement="bottomLeft" slot="extra">
-            <a class="ant-dropdown-link" href="#">
-              <a-icon type="ellipsis" />
-            </a>
-            <a-menu slot="overlay">
-              <a-menu-item>
-                <a href="javascript:;">操作一</a>
-              </a-menu-item>
-              <a-menu-item>
-                <a href="javascript:;">操作二</a>
-              </a-menu-item>
-            </a-menu>
-          </a-dropdown>
-          <ve-ring :data="chartData"></ve-ring>
-        </a-card>
-      </a-col>
-      <a-col :xl="12" :lg="24" :md="24" :sm="24" :xs="24">
-        <a-card class="antd-pro-pages-dashboard-analysis-salesCard" :loading="loading" :bordered="false" title="销售额类别占比" :style="{ marginTop: '24px', minHeight: '500px' }">
-          <div slot="extra" style="height: inherit;">
+      <a-col >
+        <a-card class="antd-pro-pages-dashboard-analysis-salesCard" :loading="loading" :bordered="true" title="选课学生" :style="{ marginTop: '24px', minHeight: '50px' }">
+          <!-- <div slot="extra" style="height: inherit;"> -->
             <!-- style="bottom: 12px;display: inline-block;" -->
-            <span class="dashboard-analysis-iconGroup">
+            <!-- <span class="dashboard-analysis-iconGroup">
               <a-dropdown :trigger="['click']" placement="bottomLeft">
                 <a-icon type="ellipsis" class="ant-dropdown-link" />
                 <a-menu slot="overlay">
@@ -35,26 +16,56 @@
                   </a-menu-item>
                 </a-menu>
               </a-dropdown>
-            </span>
-            <div class="analysis-salesTypeRadio">
+            </span> -->
+            <!-- <div class="analysis-salesTypeRadio">
               <a-radio-group defaultValue="a">
                 <a-radio-button value="a">全部渠道</a-radio-button>
                 <a-radio-button value="b">线上</a-radio-button>
                 <a-radio-button value="c">门店</a-radio-button>
               </a-radio-group>
-            </div>
-
-          </div>
-          <h4>销售额</h4>
-          <div style="width: calc(100% - 240px);">
+            </div> -->
+          <!-- </div> -->
+          <div>
+              <!-- style="width: calc(100% - 240px);" -->
             <div>
-              <ve-pie :data="chartData"></ve-pie>
+              <ve-pie :data="stuData">
+                对于这个图的提示，我只是测试一下可以把文字位置
+              </ve-pie>
             </div>
 
           </div>
         </a-card>
       </a-col>
-    </a-row>
+
+      <a-col >
+        <a-card :style="{ marginTop: '24px' }">
+          <!-- <a-dropdown :trigger="['click']" placement="bottomLeft" slot="extra">
+            <a class="ant-dropdown-link" href="#">
+              <a-icon type="ellipsis" />
+            </a>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a href="javascript:;">操作一</a>
+              </a-menu-item>
+              <a-menu-item>
+                <a href="javascript:;">操作二</a>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown> -->
+          <div class="left-graph">
+            <h4>课程题目总数</h4>
+            <ve-ring :data="problemData">
+            </ve-ring>
+          </div>
+          <div class="right-graph">
+            <h4>AC次数</h4>
+            <ve-ring :data="acData">
+            </ve-ring>
+          </div>
+        </a-card>
+      </a-col>
+      <!-- :xl="12" :lg="24" :md="24" :sm="24" :xs="24" -->
+      
   </div>
 </template>
 
@@ -62,15 +73,31 @@
    export default {
     data () {
       return {
-        chartData: {
-          columns: ['日期', '访问用户'],
+        stuData: {
+          columns: ['年份', '人数'],
           rows: [
-            { '日期': '1/1', '访问用户': 1393 },
-            { '日期': '1/2', '访问用户': 3530 },
-            { '日期': '1/3', '访问用户': 2923 },
-            { '日期': '1/4', '访问用户': 1723 },
-            { '日期': '1/5', '访问用户': 3792 },
-            { '日期': '1/6', '访问用户': 4593 }
+            { '年份': '2015', '人数': 1393 },
+            { '年份': '2016', '人数': 3530 },
+            { '年份': '2017', '人数': 2923 },
+            { '年份': '2018', '人数': 1723 },
+            { '年份': '2019', '人数': 3792 }
+          ]
+        },
+        problemData: {
+          columns: ['课程名称', '题目总数'],
+          rows: [
+            { '课程名称': '课程1', '题目总数': 1393 },
+            { '课程名称': '课程2', '题目总数': 3530 },
+            { '课程名称': '课程3', '题目总数': 3530 },
+            { '课程名称': '课程4', '题目总数': 3530 },
+            { '课程名称': '课程5', '题目总数': 3530 },
+          ]
+        },
+        acData: {
+          columns: ['是否ac', '次数'],
+          rows: [
+            { '是否ac': 'ac次数', '次数': 1393 },
+            { '是否ac': '未ac次数', '次数': 3530 },
           ]
         }
       }
@@ -88,8 +115,17 @@
       margin-right: 24px;
 
       a {
-        margin-left: 24px;
+        margin-left: 20px;
       }
     }
+  }
+  .left-graph {
+      width : 40%;
+      float: left;
+  }
+  .right-graph {
+      width : 40%;
+      float: right;
+      margin-right:5%;
   }
 </style>
