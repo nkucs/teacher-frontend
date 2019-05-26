e6791e4df298b69da9c0fb6cbec6e70d3495ddc0
baoayong <baoayong@qq.com>
2019-05-26 23:59:43 +0800

    完成学生统计页面的学生不同时间短用时柱状图。Ref #97

---
+++ src/views/stat/StatStudentData.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <stattimedistribution></stattimedistribution>
+  </div>
+</template>
+
+<script>
+import stattimedistribution from './StatTimeDistribution'
+export default {
+  components: {
+    stattimedistribution
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
