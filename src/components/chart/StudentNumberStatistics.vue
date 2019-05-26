43046f288a9e49f3d31ca399a2a8bd2b275965ea
tinzhao007 <tianzhao007@gmail.com>
2019-05-26 19:34:38 +0800

    Add new chart for student number statistics. Ref #99 with '#' will be ignored, and an empty message aborts the commit.

---
+++ src/components/chart/StudentNumberStatistics.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ve-bar :data="chartData"></ve-bar>
+  </div>  
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        chartData: {
+          columns: ['年份', '人数'],
+          rows: [
+            { 年份: '2015', 人数: 1393 },
+            { 年份: '2016', 人数: 3530 },
+            { 年份: '2017', 人数: 2923 },
+            { 年份: '2018', 人数: 1723 },
+            { 年份: '2019', 人数: 3792 }
+          ]
+        }
+      }
+    }
+  }
+</script>
