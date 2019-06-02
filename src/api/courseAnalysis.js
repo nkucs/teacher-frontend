683d7708f55f396475a8655ce2957f2eaaa7e1ee
chenbin <ertyui2003@163.com>
2019-06-02 13:48:34 +0800

    课程内题目的提交次数后端实现 蹇坤霖 #149

--- src/api/courseAnalysis.js
+++ src/api/courseAnalysis.js
@@ -1,18 +1,30 @@
 import { axios } from '@/utils/request'
 
 const apis = {
-  wordlist: '/teacher/submission/statistics/get-wordcloud' // [映射] url名称: 实际url
+  wordlist: '/teacher/submission/statistics/get-wordcloud',
+  getStatisticTable: '/teacher/submission/statistics/get_submission_info'   // [映射] url名称: 实际url
   // 此处实际url: https://httpbin.org/anything
   // 基地址baseURL在vue.config.js中指定
 }
 
-export function wordcloud() { // 其他文件使用本接口时调用test
+export function wordcloud(parameter) { // 其他文件使用本接口时调用test
   return axios({
     url: apis.wordlist, // 此处url为上面定义的url
     method: 'get', // 一般 get 或 post
-    // params: {
-    //     'sub-id': parameter
-    // } // get方法
+    params: {
+        'course_id': parameter
+    } // get方法
+    // data: parameter // post方法
+  })
+}
+
+export function problemTables(parameter) {
+  return axios({
+    url: apis.getStatisticTable, // 此处url为上面定义的url
+    method: 'get', // 一般 get 或 post
+    params: {
+        'course_id': parameter
+    } // get方法
     // data: parameter // post方法
   })
 }
