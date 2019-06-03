da687a742ae8a6be4b9b355451f93ab137c96e0f
tinzhao007 <tianzhao007@gmail.com>
2019-06-03 20:51:37 +0800

    Finish student number statistics frontend. Ref #193

--- src/api/distribution.js
+++ src/api/distribution.js
@@ -4,7 +4,8 @@ const apis = {
   distribution: '/distribution/get-all-distribution', // [映射] url名称: 实际url
   // 此处实际url: https://httpbin.org/anything
   // 基地址baseURL在vue.config.js中指定
-  runtimes: '/distribution/get-runtime-ratio'
+  runtimes: '/distribution/get-runtime-ratio',
+  studentnumberstatistics: '/teacher/course/stat/get-course-student-number'
 }
 
 export function distribution(parameter) { // 其他文件使用本接口时调用test
@@ -21,4 +22,13 @@ export function runtimes(parameter) { // 其他文件使用本接口时调用tes
     method: 'get', // 一般 get 或 post
     params: parameter // get方法
     // data: parameter // post方法
-  }) }
+  })
+}
+export function studentnumberstatistics(parameter) { // 其他文件使用本接口时调用test
+  return axios({
+    url: apis.studentnumberstatistics, // 此处url为上面定义的url
+    method: 'get', // 一般 get 或 post
+    params: parameter // get方法
+    // data: parameter // post方法
+  })
+}
